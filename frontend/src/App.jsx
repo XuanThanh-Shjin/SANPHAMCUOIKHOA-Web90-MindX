1e34858372b34a525e37dac5bd2f1daa5e7f9675
Shjinn <thanh03951@example.com>
2025-09-23 15:45:58 +0700

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/home/Home'
+import List from './pages/list/List'
+import Hotel from './pages/hotel/Hotel'
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/hotels" element={<List />} />
+      <Route path="/hotels/:id" element={<Hotel />} />
+    </Routes>
+  )
+}
+
+export default App
